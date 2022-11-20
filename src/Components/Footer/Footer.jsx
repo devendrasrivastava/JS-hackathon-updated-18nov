9edfeae4595c603a20166536d2baa0c2ffd615e8
Kumard <kumard@rbos.co.uk>
2022-11-20 15:39:30 +0530

    signup page amendments done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div>
 
 
-    <footer class="site-footer">
+    <footer class="site-footer my-4">
       <div class="container">
         <div class="row">
           <div class="col-sm-12 col-md-6">
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,127 +1,135 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './SignUp.css'
-
-import {useFormik} from 'formik'
+import { useFormik } from 'formik'
 import * as yup from 'yup'
 import Header from '../Header/Header';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
 
-   
 
-    const formik=useFormik({
-        initialValues:{
-            firstname:"",
-            lastname:"",
-            email:"",
-            city:"",
-            phone:"",
-            age:"",
-            password:"",
-            confirmpassword:""
+
+    const formik = useFormik({
+        initialValues: {
+            firstname: "",
+            lastname: "",
+            email: "",
+            city: "",
+            phone: "",
+            age: "",
+            password: "",
+            confirmpassword: ""
         },
-        onSubmit:values =>{
+        onSubmit: values => {
             // console.log(values);
-            fetch("http://localhost:9000/auth/register",{
-                method:'POST',
-                headers:{
-                    "Content-Type":"application/json"
+            fetch("http://localhost:9000/auth/register", {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
                 },
-                body:JSON.stringify(values)
+                body: JSON.stringify(values)
             })
-            .then(res=>res.json())
-            .then(data=>
-                {
+                .then(res => res.json())
+                .then(data => {
                     console.log(data)
                     console.log(data.message)
                 })
-            .catch((e)=>console.log("Catch block executed"))
-            .finally(console.log("Finally executed"))
+                .catch((e) => console.log("Catch block executed"))
+                .finally(console.log("Finally executed"))
         },
-        validationSchema:yup.object().shape({
-            firstname:yup.string()
-            .min(3,"First name is too short")
-            .max(10,"First name is too long")
-            .required("Firstname cannot be left blank"),
-            lastname:yup.string()
-            .min(3,"Last name is too short")
-            .max(10,"Last name is too long")
-            .required("lastname cannot be left blank"),
-            email:yup.string()
-            .email("Invalid email address")
-            .required("Email cannot be left blank"),
-            city:yup.string()
-            .required("City cannot be left blank"),
-            phone:yup.string()
-            .required("Phone cannot be left blank"),
-            age:yup.string()
-            .required("Age cannot be left blank"),
-            password:yup.string()
-            .required("Password cannot be left blank"),
-            confirmpassword:yup.string()
-            .required("Confirm Password cannot be left blank")
-            .test("confirmpassword","Password and Confirm password should be same",function (cpass) {
-                if(this.parent.password===cpass){
-                    return true;
-                }
+        validationSchema: yup.object().shape({
+            firstname: yup.string()
+                .min(3, "First name is too short")
+                .max(20, "First name is too long")
+                .required("Firstname cannot be left blank"),
+            lastname: yup.string()
+                .min(3, "Last name is too short")
+                .max(20, "Last name is too long")
+                .required("lastname cannot be left blank"),
+            email: yup.string()
+                .email("Invalid email address")
+                .required("Email cannot be left blank"),
+            city: yup.string()
+                .required("City cannot be left blank"),
+            phone: yup.string()
+                .required("Phone cannot be left blank"),
+            age: yup.string()
+                .required("Age cannot be left blank"),
+            password: yup.string()
+                .required("Password cannot be left blank"),
+            confirmpassword: yup.string()
+                .required("Confirm Password cannot be left blank")
+                .test("confirmpassword", "Password and Confirm password should be same", function (cpass) {
+                    if (this.parent.password === cpass) {
+                        return true;
+                    }
                     return false;
-            })
+                })
         })
     })
-  return (
-    <>
-    <Header/>
-    <div className="container">
-        
-        <div className="row">
-            <div className="col-md-4 offset-md-4" id="signUpForm">
-                <div className="bg-light text-light py-3 mt-3 text-center rounded">
-                <img src="./assets/nw logo.png" alt="" class="img-fluid"/>
-                    
-                </div>
-                <form onSubmit={formik.handleSubmit}>
-                    <div className="mt-2">
-                    <input id="firstname" name='firstname' type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.firstname} className="form-control form-control-sm" placeholder="First Name"/>
-                    {formik.errors.firstname && formik.touched.firstname ? <span className='text-danger'>{formik.errors.firstname}</span>:null}
-                    </div>
-                    <div className="mt-2">
-                    <input id="lastname" name='lastname' type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.lastname} className="form-control form-control-sm" placeholder="Last Name"/>
-                    {formik.errors.lastname  && formik.touched.lastname? <span className='text-danger'>{formik.errors.lastname}</span>:null}
-                    </div>
-                    <div className="mt-2">
-                    <input id="email" name='email' type="email" onChange={formik.handleChange} onBlur={formik.handleBlur}className="form-control  form-control-sm" value={formik.values.email} placeholder="Email"/>
-                    {formik.errors.email && formik.touched.email? <span className='text-danger'>{formik.errors.email}</span>:null}
-                    </div>
-                    <div className="mt-2">
-                    <input id="city" name='city' type="text" onChange={formik.handleChange}onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.city}placeholder="city"/>
-                    {formik.errors.city && formik.touched.city? <span className='text-danger'>{formik.errors.city}</span>:null}
-                    </div>
-                    <div className="mt-2">
-                    <input id="phone" name='phone' type="number" onChange={formik.handleChange}onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.phone} placeholder="Phone"/>
-                    {formik.errors.phone && formik.touched.phone? <span className='text-danger'>{formik.errors.phone}</span>:null}
-                    </div>
-                    <div className="mt-2">
-                    <input id="age" name='age' type="number" onChange={formik.handleChange}onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.age} placeholder="Age"/>
-                    {formik.errors.age && formik.touched.age? <span className='text-danger'>{formik.errors.age}</span>:null}
-                    </div>
-                    <div className="mt-2">
-                    <input id="password" name='password' type="password" onChange={formik.handleChange}onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.password} placeholder="Password"/>
-                    {formik.errors.password && formik.touched.password? <span className='text-danger'>{formik.errors.password}</span>:null}
-                    </div>
-                    <div className="mt-2">
-                    <input id="confirmpassword" name='confirmpassword' type="password" onChange={formik.handleChange}onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.confirmpassword} placeholder="Confirm Password"/>
-                    {formik.errors.confirmpassword && formik.touched.confirmpassword? <span className='text-danger'>{formik.errors.confirmpassword}</span>:null}
-                    </div>
-                    <div className="mt-2 ">
-                    <button type="submit" id="submitbutton" className='btn btn-success'>Submit</button>
-                    {/* <button type="reset" className='btn btn-warning'>Reset</button> */}
-                    </div>
+    return (
+        <>
+            <Header />
+            <div className="container">
 
-                   
-                </form>
+                <div className="row">
+                    <div className="col-md-4 offset-md-4" id="signUpForm">
+                        <div className="bg-light text-light py-3 mt-3 text-center rounded">
+                            <img src="./assets/nw logo.png" alt="" class="img-fluid" />
+
+                        </div>
+                        <form onSubmit={formik.handleSubmit}>
+                            <div className="mt-2">
+                                <input id="firstname" name='firstname' type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.firstname} className="form-control form-control-sm" placeholder="First Name" />
+                                {formik.errors.firstname && formik.touched.firstname ? <span className='text-danger'>{formik.errors.firstname}</span> : null}
+                            </div>
+                            <div className="mt-2">
+                                <input id="lastname" name='lastname' type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.lastname} className="form-control form-control-sm" placeholder="Last Name" />
+                                {formik.errors.lastname && formik.touched.lastname ? <span className='text-danger'>{formik.errors.lastname}</span> : null}
+                            </div>
+                            <div className="mt-2">
+                                <input id="email" name='email' type="email" onChange={formik.handleChange} onBlur={formik.handleBlur} className="form-control  form-control-sm" value={formik.values.email} placeholder="Email" />
+                                {formik.errors.email && formik.touched.email ? <span className='text-danger'>{formik.errors.email}</span> : null}
+                            </div>
+                            <div className="mt-2">
+                                <input id="city" name='city' type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.city} placeholder="city" />
+                                {formik.errors.city && formik.touched.city ? <span className='text-danger'>{formik.errors.city}</span> : null}
+                            </div>
+                            <div className="mt-2">
+                                <input id="phone" name='phone' type="number" onChange={formik.handleChange} onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.phone} placeholder="Phone" />
+                                {formik.errors.phone && formik.touched.phone ? <span className='text-danger'>{formik.errors.phone}</span> : null}
+                            </div>
+                            <div className="mt-2">
+                                <input id="age" name='age' type="number" onChange={formik.handleChange} onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.age} placeholder="Age" />
+                                {formik.errors.age && formik.touched.age ? <span className='text-danger'>{formik.errors.age}</span> : null}
+                            </div>
+                            <div className="mt-2">
+                                <input id="password" name='password' type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.password} placeholder="Password" />
+                                {formik.errors.password && formik.touched.password ? <span className='text-danger'>{formik.errors.password}</span> : null}
+                            </div>
+                            <div className="mt-2">
+                                <input id="confirmpassword" name='confirmpassword' type="password" onChange={formik.handleChange} onBlur={formik.handleBlur} className="form-control form-control-sm" value={formik.values.confirmpassword} placeholder="Confirm Password" />
+                                {formik.errors.confirmpassword && formik.touched.confirmpassword ? <span className='text-danger'>{formik.errors.confirmpassword}</span> : null}
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" required />
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    I Accept the Terms and Conditions
+                                </label>
+                            </div>
+                            <div className="mt-2">
+                                <button type="submit" id="submitbutton" className='btn btn-success'>Submit</button>
+                            </div>
+                            <Link to="/loginpage">Already Signed up?</Link>
+                            <div className="mt-2 ">
+                                {/* <button type="reset" id="resetbutton" className='btn btn-success'>Reset</button> */}
+                            </div>
+
+
+                        </form>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
